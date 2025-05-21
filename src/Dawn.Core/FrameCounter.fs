00cc262cdc2c9bd48ef9b4827de7e35e1ce27c02
Isobel Cullen <646896+isobel-cullen@users.noreply.github.com>
2025-05-21 14:31:05 +0100

    add a DrawableGameCompontent that shows the fps
    It works! Probably not the best mechanism, no-one else seems to use the
    built-in component stuff much.

---
+++ src/Dawn.Core/FrameCounter.fs
@@ -0,0 +1,26 @@
+﻿namespace Dawn.Core
+
+open FontStashSharp
+open System.Diagnostics
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+type FrameCounter (game, fonts: FontSystem, sprites: SpriteBatch) =
+    inherit DrawableGameComponent (game)
+
+    let buffer = CircularBuffer.create 60
+    let font24 = fonts.GetFont(24f)
+    let mutable framerate = 0.0
+
+    override self.LoadContent (): unit = 
+        base.LoadContent()
+
+    override self.Draw (gameTime: GameTime): unit = 
+        if buffer.Push gameTime.ElapsedGameTime.TotalSeconds then
+            framerate <- truncate (1.0 / (Seq.average buffer))
+
+        sprites.Begin ()
+        sprites.DrawString(font24, (string framerate), Vector2.Zero, Color.Violet) |> ignore
+        sprites.End ()
+
+        base.Draw(gameTime: GameTime)
