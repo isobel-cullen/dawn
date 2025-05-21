4676f1c20f58a468eedf5f48a4b4b9ccb02b9610
Isobel Cullen <646896+isobel-cullen@users.noreply.github.com>
2025-05-21 16:19:42 +0100

    cellular atomata rule 110 renderer

---
+++ src/Dawn.Core/Cellular.fs
@@ -0,0 +1,81 @@
+﻿module Cellular
+module Types =
+    open System
+    open System.Text
+    open FSharp.Core.Printf
+
+    type Cell =
+    | O
+    | X
+        override x.ToString() =
+            match x with
+            | O -> "O"
+            | X -> "X"
+
+    let randomCell (random: Random) _ =
+        match random.Next(2) with
+        | 0 -> O
+        | 1 -> X
+        | _ -> failwith "Maths is broken"
+
+    type Rule = Cell * Cell * Cell -> Cell
+
+    type Row = {
+        State: Cell array
+        Iteration: int64
+    } with
+        static member Initialize size =
+            let r = Random()
+            { 
+                State = Array.init size (randomCell r)
+                Iteration = 0L
+            }
+
+        member this.Evolve (rule: Rule) =
+            let row = this.State
+            let getNeighbours index (cell: Cell)=
+                match index, row.Length with
+                | _ when index = 0 ->
+                    (row.[row.Length - 1], cell, row.[index + 1])
+                | _ when index = (row.Length - 1) ->
+                    (row.[index - 1], cell, row.[0])
+                | _ -> 
+                    (row.[index - 1], cell, row.[index + 1])
+            {
+                State = row |> Array.mapi (fun i c -> getNeighbours i c |> rule)
+                Iteration = this.Iteration + 1L
+            }
+
+        member this.Print () =
+            let buffer = StringBuilder(this.State.Length)
+            this.State |> Array.iter (bprintf buffer "%O")
+            buffer.ToString ()
+
+module Rules =
+    open Types
+    let Rule110 (cells: Cell * Cell * Cell) =
+        match cells with
+        | X,X,X -> O
+        | X,X,O -> X
+        | X,O,X -> X
+        | X,O,O -> O
+        | O,X,X -> X
+        | O,X,O -> X
+        | O,O,X -> X
+        | O,O,O -> O
+
+    let namedRule (name: int32) (cells: Cell * Cell * Cell) =
+        let isSet c =
+            match name &&& c with
+            | 0 -> O
+            | _ -> X
+
+        match cells with
+        | X,X,X -> isSet 128
+        | X,X,O -> isSet 64
+        | X,O,X -> isSet 32
+        | X,O,O -> isSet 16
+        | O,X,X -> isSet 8
+        | O,X,O -> isSet 4
+        | O,O,X -> isSet 2
+        | O,O,O -> isSet 1
--- src/Dawn.Core/DawnGame.fs
+++ src/Dawn.Core/DawnGame.fs
@@ -1,5 +1,8 @@
 ﻿namespace Dawn.Core
 
+open Cellular.Types
+open Cellular.Rules
+
 open System
 open System.IO
 open System.Diagnostics
@@ -19,6 +22,11 @@ type DawnGame () as self =
     let fontSystem = new FontSystem()
     let mutable spriteBatch: SpriteBatch = Unchecked.defaultof<SpriteBatch>
 
+    let rows = CircularBuffer (720 / 32)
+    let mutable cells = Row.Initialize (1280 / 32)
+    let mutable sinceLastUpdate =  TimeSpan.Zero
+    let mutable font: DynamicSpriteFont = Unchecked.defaultof<DynamicSpriteFont>
+
     do
         gdm.PreferredBackBufferWidth    <- 1280
         gdm.PreferredBackBufferHeight   <- 720
@@ -26,15 +34,18 @@ type DawnGame () as self =
     override self.Initialize () = 
         base.IsMouseVisible <- false
         base.IsFixedTimeStep <- true
-        base.Window.AllowUserResizing <- true
 
         fontSystem.AddFont(File.ReadAllBytes(@"Content\clover-sans.ttf"))
 
         // dirty :(
         spriteBatch <-  new SpriteBatch(gdm.GraphicsDevice)
+        font <- fontSystem.GetFont(32f)
 
         let fps = new FrameCounter (self, fontSystem, spriteBatch)
         base.Components.Add fps
+
+        while not (rows.Push (cells.Print ())) do
+            cells <- cells.Evolve Rule110
 
         base.Initialize ()
 
@@ -47,12 +58,24 @@ type DawnGame () as self =
     override self.Update (gameTime: GameTime): unit = 
         let keyboard = Keyboard.GetState ()
 
-        for key in keyboard.GetPressedKeys () do
-            Console.WriteLine(key.ToString() + " was pressed")
+        if keyboard.[Keys.Escape] = KeyState.Down then
+            base.Exit ()
 
+        sinceLastUpdate <- sinceLastUpdate + gameTime.ElapsedGameTime
+        if sinceLastUpdate >= TimeSpan.FromSeconds 1.0 then
+            cells <- cells.Evolve Rule110
+            rows.Push (cells.Print ()) |> ignore
+            sinceLastUpdate <- TimeSpan.Zero
+        
         base.Update(gameTime: GameTime)
 
     override self.Draw (gameTime: GameTime) =
         base.GraphicsDevice.Clear (Color.LightSeaGreen)
+
+        spriteBatch.Begin ()
+        Seq.iteri (fun i r ->
+            let vec = Vector2(0f, float32 i * 32f)
+            spriteBatch.DrawString(font, string r, vec, Color.Tomato) |> ignore) rows
+        spriteBatch.End()
 
         base.Draw gameTime
